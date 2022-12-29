4545c3a084d2dd400300b23bef401e249ae16583
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2022-12-29 21:29:04 +0000

    challenge 1 add;

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/challenge_1.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/challenge_1.js
@@ -5,10 +5,10 @@ const avgKoalas = calcAverage(23, 34, 27);
 
 function checkWinner (avgDolphins, avgKoalas) {
     if (avgDolphins >= avgKoalas * 2){
-        console.log(`Delphins win (${avgDolphins} vs ${avgKoalas}).`) 
+        console.log(`Delphins win (${avgDolphins} vs ${avgKoalas}).`); 
     } else if (avgKoalas >= avgDolphins * 2) {
-        console.log(`Koalas win (${avgKoalas} vs ${avgDolphins}).`)
-    } else console.log(`nobody won`)
+        console.log(`Koalas win (${avgKoalas} vs ${avgDolphins}).`);
+    } else console.log(`nobody won`);
 }
 
 checkWinner(avgDolphins, avgKoalas)

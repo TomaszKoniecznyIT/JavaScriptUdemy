6658364d5f8491c700ff136221db9d7d127c9dbc
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-03 16:43:01 +0000

    challange 43 object

---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/challenge_42.js
@@ -0,0 +1,9 @@
+const jonas = {
+    firstName: "Jonas",
+    lastName: "Schmedtmann",
+    age: 2037-1991,
+    job: "teacher",
+    friends: ['Michael', 'Peter', 'John']
+}
+
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends and best frind is called ${jonas.friends[0]}.`)

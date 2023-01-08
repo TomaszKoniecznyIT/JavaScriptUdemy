1e9090f984ddb46bae3e7465c1f91581a852fec5
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-08 09:30:47 +0000

    object

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/challenge_44.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/challenge_44.js
@@ -2,15 +2,22 @@ const jonas1 = {
     firstName: "Jonas",
     lastName: "Schmedtmann",
     birthYear: 1991,
+    job: "teacher",
     friends: ["Michael", "Peter", "Steven"],
-    hasDriverLicen: true,
+    hasDriverLicen: false,
 
     calcAge1: function(){
         this.age = 2037 - this.birthYear; // creates age
         return this.age;
+    },
+
+    getSummary: function(){
+        return `${this.firstName} is a ${this.calcAge1()}-year old ${this.job}, has ${this.hasDriverLicen ? "a" : "no"} driver's license.`
     }
 };
 
 console.log(jonas1.calcAge1());  // must be done to create an age
 console.log(jonas1.age);
 console.log(jonas1['age']);
+
+console.log(jonas1.getSummary())

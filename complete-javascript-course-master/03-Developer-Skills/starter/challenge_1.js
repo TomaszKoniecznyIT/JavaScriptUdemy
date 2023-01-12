17377b22e5e68264aa32815dac7c84ce4fd82e2a
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-12 17:07:40 +0000

    challenge 1

---
+++ complete-javascript-course-master/03-Developer-Skills/starter/challenge_1.js
@@ -0,0 +1,13 @@
+function printForecast(array) {
+  let forecast = "... ";
+  let oneDayForecast = "";
+  for (let i = 0; i < 3; i++) {
+    oneDayForecast = `${array[i]} degrees Celsius in ${i + 1} days ...`;
+    forecast += oneDayForecast;
+    oneDayForecast = "";
+  }
+  return forecast;
+}
+
+const temps = [17, 21, 23];
+console.log(printForecast(temps));

2528d1dfbd853629585066a227f372d30443706d
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-12 14:15:08 +0000

    challenge 59 - Jonas code

--- complete-javascript-course-master/03-Developer-Skills/starter/challenge_59.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/challenge_59.js
@@ -24,3 +24,23 @@ function amplitudeArray(array) {
 
 const amplitude = amplitudeArray(errorElimination(temperatures));
 console.log(amplitude);
+
+// Jonas code
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitude1 = calcTempAmplitude(temperatures);
+console.log(amplitude1);

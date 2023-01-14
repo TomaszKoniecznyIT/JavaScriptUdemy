b275fe5f0d7ec1e78c6cbffd68f360fdc948beb5
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-14 18:43:36 +0000

    score

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,10 +1,23 @@
 'use strict';
 
-const number = Math.trunc(Math.random() * 20) + 1;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let currentScore = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!!!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number !!!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too high !';
+    currentScore--;
+    document.querySelector('.score').textContent = currentScore;
+  } else {
+    document.querySelector('.message').textContent = 'Too low !';
+    currentScore--;
+    document.querySelector('.score').textContent = currentScore;
   }
 });

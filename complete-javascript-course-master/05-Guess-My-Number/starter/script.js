9b99510652fa42b6f37675f01107e484fcf408b7
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-15 06:56:15 +0000

    comments

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,25 +1,38 @@
 'use strict';
 
+// drawing a number
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = secretNumber;
 
+// setting max points to 20
 let currentScore = 20;
 
+// action after pressing the check button!
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
+
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!!!';
+
+    // When player win
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number !!!';
+
+    // When guess is too high
   } else if (guess > secretNumber) {
     document.querySelector('.message').textContent = 'Too high !';
     currentScore--;
     document.querySelector('.score').textContent = currentScore;
+
+    // When guess is too low
   } else {
     document.querySelector('.message').textContent = 'Too low !';
     currentScore--;
     document.querySelector('.score').textContent = currentScore;
   }
+
+  // information about losing the game when the score is 0
   if (!currentScore) {
     document.querySelector('.message').textContent = 'You lost the game !!!';
   }

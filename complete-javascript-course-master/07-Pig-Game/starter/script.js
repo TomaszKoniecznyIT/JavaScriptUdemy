8a4852a5fd9655b91dcfef3d6ba8f03c6c2f3406
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-18 13:26:30 +0000

    hidden dice

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -23,6 +23,7 @@ diceEl.classList.add('hidden');
 let totaleScore = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -34,45 +35,51 @@ const switchPlayer = function () {
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
-  // 1. Generating a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  // 2. Display dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+    // 2. Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
 
-  // 3. Check for rolled 1
-  if (dice !== 1) {
-    // Add dice to current score
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    // Switch to next player
-    switchPlayer();
+    // 3. Check for rolled 1
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch to next player
+      switchPlayer();
+    }
   }
 });
 
 // Holding current score
 btnHold.addEventListener('click', function () {
-  // 1. Add current score to active player's score
-  totaleScore[activePlayer] += currentScore;
-  document.getElementById(`score--${activePlayer}`).textContent =
-    totaleScore[activePlayer];
+  if (playing) {
+    // 1. Add current score to active player's score
+    totaleScore[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      totaleScore[activePlayer];
 
-  // 2. Check if player's score >= 100
-  if (totaleScore[activePlayer] >= 13) {
-    // Finish the game
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.add('player--winner');
-    console.log(document.querySelector(`.player--${activePlayer}`));
-    //if we don't do it, both settings for these classes will be active (nothing will change in appearance, because the last class is player--winner)
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.remove('player--active');
-  } else {
-    // Switch to the next player
-    switchPlayer();
+    // 2. Check if player's score >= 100
+    if (totaleScore[activePlayer] >= 13) {
+      // Finish the game
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      console.log(document.querySelector(`.player--${activePlayer}`));
+      //if we don't do it, both settings for these classes will be active (nothing will change in appearance, because the last class is player--winner)
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+      diceEl.classList.add('hidden');
+    } else {
+      // Switch to the next player
+      switchPlayer();
+    }
   }
 });

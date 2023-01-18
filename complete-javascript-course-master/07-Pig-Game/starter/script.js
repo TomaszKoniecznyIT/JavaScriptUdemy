9e03359d07dd0627d0f9bbb7f8867203d1fd0537
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-18 11:40:03 +0000

    switch player

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -3,18 +3,25 @@
 // Selecting elements
 const score0El = document.querySelector('#score--0');
 const currentScore0El = document.querySelector('#current--0');
+const player0El = document.querySelector('.player--0');
 
 const scote1El = document.getElementById('score--1');
-const currentScote1El = document.getElementById('current-1');
+const currentScore1El = document.getElementById('current--1');
+const player1El = document.querySelector('.player--1');
 
 const diceEl = document.querySelector('.dice');
+
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+let activePlayer = 0;
+
 // Starting conditions
 score0El.textContent = 0;
 scote1El.textContent = 0;
+let totaleScore = [0, 0];
+
 diceEl.classList.add('hidden');
 
 let currentScore = 0;
@@ -32,8 +39,15 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    currentScore0El.textContent = currentScore; // CHANGE LATER
+    console.log(currentScore, typeof currentScore);
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });

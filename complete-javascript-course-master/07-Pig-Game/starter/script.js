92fbc7eba59e8bfc74a3d843f6eaecda37660233
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-18 10:19:55 +0000

    pig game selecting elements, starting conditions

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const scote1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--rol');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting conditions
+score0El.textContent = 0;
+scote1El.textContent = 0;
+diceEl.classList.add('hidden');

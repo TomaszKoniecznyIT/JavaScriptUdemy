f89c3f105c296422d819d10086e846601715ef59
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-18 19:07:27 +0000

    moving all startup settings to the init function

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -15,15 +15,20 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let totaleScore, currentScore, activePlayer, playing;
 
-let totaleScore = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  totaleScore = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  currentScore0El.textContent = 0;
+  currentScore1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.add('player--active');
+};
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -33,6 +38,7 @@ const switchPlayer = function () {
   player1El.classList.toggle('player--active');
 };
 
+init();
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   if (playing) {
@@ -88,13 +94,5 @@ btnNew.addEventListener('click', function () {
   document
     .querySelector(`.player--${activePlayer}`)
     .classList.remove('player--winner');
-  player0El.classList.add('player--active');
-  totaleScore = [0, 0];
-  currentScore = 0;
-  activePlayer = 0;
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  currentScore0El.textContent = 0;
-  currentScore1El.textContent = 0;
-  playing = true;
+  init();
 });

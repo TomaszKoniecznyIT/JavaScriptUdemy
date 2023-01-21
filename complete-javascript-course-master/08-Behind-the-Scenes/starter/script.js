4359228359b572817d0a8f487eb8b5a5e7bff2db
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-21 23:16:26 +0000

    scoping in practice 3

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -4,8 +4,26 @@ function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
   function printAge() {
-    const output = `You are ${age}, born in ${birthYear}`;
+    const output = `${firstName} are ${age}, born in ${birthYear}`;
     console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      // will change to Steven but only for this block
+      // Jonas will still be there
+      const firstName = 'Steven';
+      const str = `Oh, and you're a millenial ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // there will no longer be access to the str variable
+    // console.log(str);
+    console.log(millenial);
+    // in strict mode the function will not work, but in normal mode it will
+    // console.log(add(2, 3));
   }
   printAge();
 
@@ -14,3 +32,7 @@ function calcAge(birthYear) {
 
 const firstName = 'Jonas';
 calcAge(1991);
+// there will no longer be access to the age variable or the printAge function
+// printAge();
+// console.log(age);
+// console.log(millenial);

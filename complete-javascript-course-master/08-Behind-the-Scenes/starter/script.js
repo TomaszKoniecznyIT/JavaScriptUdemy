3c0fd7bf84aff380f2797ae981c8e02e8332429c
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-21 22:04:02 +0000

    scoping in practice 2

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -2,7 +2,13 @@
 
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
-  console.log(firstName);
+
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
+
   return age;
 }
 

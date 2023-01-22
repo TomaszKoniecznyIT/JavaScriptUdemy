186969c9968e1174f8f5ae8b7f682705229af708
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-22 18:50:58 +0000

    hoisting 3

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -54,3 +54,11 @@ var numProd = 10;
 function delShopCart() {
   console.log('All products deleted');
 }
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x); // true
+console.log(y === window.y); // false
+console.log(z === window.z); //false

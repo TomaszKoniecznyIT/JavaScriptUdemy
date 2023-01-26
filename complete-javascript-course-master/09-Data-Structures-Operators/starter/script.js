31b5d2ff55bdc798bbcb223aecf22f053c59276d
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-26 09:19:59 +0000

    destructuring array

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -27,3 +27,15 @@ const restaurant = {
     },
   },
 };
+
+// unpacking the array into variables
+const arr = [2, 3, 4];
+
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+
+console.log(a, b, c);
+console.log(x, y, z);

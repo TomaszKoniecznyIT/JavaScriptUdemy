144fefdaf162d1adbaf4a673cb4a8c41297e70cb
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-30 09:12:23 +0000

    rest pattern 2

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -207,3 +207,17 @@ console.log([...restaurant.mainMenu, ...restaurant.starterMenu]);
 const { sat, ...weekdays } = restaurant.openingHours;
 console.log(sat);
 console.log(weekdays);
+
+const add = function (...numbers) {
+  console.log(numbers);
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);

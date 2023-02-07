1f7b1b2e99cc9585ed031b0526c142d9cdf80407
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-02-07 08:36:57 +0000

    challenge 3

---
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/challenge3.js
@@ -0,0 +1,13 @@
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);

c59d95ab97ec8acb2e222dbe3e99cab547850ca3
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-02-26 13:59:00 +0000

    challenge 4 add lower case

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/challenge4.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/challenge4.js
@@ -4,9 +4,11 @@ document.body.append(document.createElement('button'));
 document.querySelector('button').addEventListener('click', function () {
   const text = document.querySelector('textarea').value;
   const textSplit = text.split('_');
-  let returnText = [textSplit[0]];
+  let returnText = [textSplit[0].toLowerCase()];
   for (let i = 1; i < textSplit.length; i++) {
-    returnText.push(textSplit[i][0].toUpperCase() + textSplit[i].slice(1));
+    returnText.push(
+      textSplit[i][0].toUpperCase() + textSplit[i].slice(1).toLowerCase()
+    );
   }
   console.log(returnText.join(''));
 });

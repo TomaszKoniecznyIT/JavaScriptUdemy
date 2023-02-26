0e08d5ca3545e154737564981e5bdaa71077b3d8
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-02-26 14:51:33 +0000

    challenge 4 change with case and boxes

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/challenge4.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/challenge4.js
@@ -3,12 +3,16 @@ document.body.append(document.createElement('button'));
 
 document.querySelector('button').addEventListener('click', function () {
   const text = document.querySelector('textarea').value;
-  const textSplit = text.split('_');
-  let returnText = [textSplit[0].toLowerCase()];
-  for (let i = 1; i < textSplit.length; i++) {
-    returnText.push(
-      textSplit[i][0].toUpperCase() + textSplit[i].slice(1).toLowerCase()
-    );
+  const textSplit = text.split('\n');
+  let box = '✅';
+  for (let i = 0; i < textSplit.length; i++) {
+    const ver = textSplit[i].trim().split('_');
+    const retVar =
+      ver[0].toLocaleLowerCase() +
+      ver[1][0].toUpperCase() +
+      ver[1].slice(1).toLowerCase();
+
+    console.log(retVar.padEnd(25, ' ') + box);
+    box += '✅';
   }
-  console.log(returnText.join(''));
 });

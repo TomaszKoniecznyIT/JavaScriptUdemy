8175cd7d2aa5b5ce4587fb7c5a4f8a3fc8bb61c8
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-03-02 19:31:40 +0000

    challenge 1 Jonas

--- complete-javascript-course-master/10-Functions/starter/challenge_1.js
+++ complete-javascript-course-master/10-Functions/starter/challenge_1.js
@@ -11,13 +11,60 @@ const poll = {
       ? this.answers[answer]++
       : alert('you have to choose between 0, 1, 2, 3');
     this.displayResults('array');
+    this.displayResults('string');
   },
   displayResults(type) {
     if (type === 'array') console.log(this.answers);
-    if (type === 'string') console.log(`Poll results are ${[...this.answers]}`);
+    if (type === 'string')
+      console.log(`Poll results are ${this.answers.join(', ')}`);
   },
 };
 
 document
   .querySelector('.poll')
   .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// Jonas
+const poll1 = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section 😃
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    // Get answer
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
+    );
+    console.log(answer);
+
+    // Register answer
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults('string');
+  },
+
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      // Poll results are 13, 2, 4, 1
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
+};
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll1.registerNewAnswer.bind(poll1));
+
+// BONUS
+// [5, 2, 3]
+// [1, 5, 3, 9, 6, 1]
+poll1.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll1.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+poll1.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });

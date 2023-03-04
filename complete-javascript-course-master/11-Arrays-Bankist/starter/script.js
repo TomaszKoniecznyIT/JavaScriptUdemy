223c5335bbbcdd96f12c33a6b9371fc484da3734
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-03-04 17:22:09 +0000

    forEach

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -74,7 +74,7 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
-// // Simple Array Methods
+// //142. Simple Array Methods
 // let arr = ['a', 'b', 'c', 'd', 'e'];
 
 // // SLICE
@@ -107,16 +107,63 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // // JOIN
 // console.log(letters.join(' - '));
 
+// ///////////////////////////////////////
+// //143. The new at Method
+// const arr = [23, 11, 64];
+// console.log(arr[0]);
+// console.log(arr.at(0));
+
+// // getting last array element
+// console.log(arr[arr.length - 1]);
+// console.log(arr.slice(-1)[0]);
+// console.log(arr.at(-1));
+
+// console.log('jonas'.at(0));
+// console.log('jonas'.at(-1));
+
+// ///////////////////////////////////////
+// //144. Looping Arrays: forEach
+// const movements2 = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// // for (const movement of movements) {
+// for (const [i, movement] of movements2.entries()) {
+//   if (movement > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+//   }
+// }
+
+// console.log('---- FOREACH ----');
+// movements2.forEach(function (mov, i, arr) {
+//   console.log(arr);
+//   if (mov > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${mov}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+//   }
+// });
+// // 0: function(200)
+// // 1: function(450)
+// // 2: function(400)
+// // ...
+
 ///////////////////////////////////////
-// The new at Method
-const arr = [23, 11, 64];
-console.log(arr[0]);
-console.log(arr.at(0));
+// forEach With Maps and Sets
+// Map
+const currencies2 = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
 
-// getting last array element
-console.log(arr[arr.length - 1]);
-console.log(arr.slice(-1)[0]);
-console.log(arr.at(-1));
+currencies2.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
 
-console.log('jonas'.at(0));
-console.log('jonas'.at(-1));
+// Set
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
+});

223a85f5bb6e4273777b25992e3157ca8307c171
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-03-08 12:04:03 +0000

    add calcDisplaySummary interest 1.2

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -295,6 +295,13 @@ const calcDisplaySummary = function (movement) {
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumOut.textContent = `${Math.abs(out)} €`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter(int => int >= 1)
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest} €`;
 };
 
 calcDisplaySummary(account1.movements);

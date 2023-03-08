7d3f0d498f029fcf255e9db32f2aef0942357f7a
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-03-08 10:35:52 +0000

    challenge 2

---
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/challenge_2.js
@@ -0,0 +1,24 @@
+const test_data_1 = [5, 2, 4, 1, 15, 8, 3];
+const test_data_2 = [16, 6, 10, 5, 6, 1, 4];
+
+const calcAverageHumanAge = function (ages) {
+  const humanAge = ages.map(age => (age <= 2 ? age * 2 : 16 + age * 4));
+  console.log(`human age :${humanAge}`);
+  const humanAgeMin18 = humanAge.filter(age => age >= 18);
+  console.log(`human age > 18 :${humanAgeMin18}`);
+  const avg =
+    humanAgeMin18.reduce((acc, age) => acc + age, 0) / humanAgeMin18.length;
+  return avg;
+};
+
+console.log(calcAverageHumanAge(test_data_1));
+
+const avg = function (ages) {
+  const humanAge = ages
+    .map(age => (age <= 2 ? age * 2 : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, _, arry) => acc + age / arry.length, 0);
+  return humanAge;
+};
+console.log(avg(test_data_2));
+console.log(avg(test_data_1));

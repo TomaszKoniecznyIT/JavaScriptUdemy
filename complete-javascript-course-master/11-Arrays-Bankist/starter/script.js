8ea010986a0e1ece56f8a7e7d68f4720b05abaf3
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-03-08 09:14:25 +0000

    filter method

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -135,6 +135,22 @@
 // );
 // console.log(movementsDescriptions);
 
+///////////////////////////////////////
+// 152. The filter Method
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const deposits = movements.filter(function (mov, i, arr) {
+  return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
+
 /////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////
 

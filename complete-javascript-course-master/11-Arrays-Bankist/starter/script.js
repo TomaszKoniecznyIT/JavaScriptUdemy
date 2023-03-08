d18c3e88b49e3783821d11a8dc63c9241c72a3b9
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-03-08 09:41:15 +0000

    reduce method

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -135,21 +135,49 @@
 // );
 // console.log(movementsDescriptions);
 
+// ///////////////////////////////////////
+// // 152. The filter Method
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const deposits = movements.filter(function (mov, i, arr) {
+//   return mov > 0;
+// });
+// console.log(movements);
+// console.log(deposits);
+
+// const depositsFor = [];
+// for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+// console.log(depositsFor);
+
+// const withdrawals = movements.filter(mov => mov < 0);
+// console.log(withdrawals);
+
 ///////////////////////////////////////
-// 152. The filter Method
+// 153. The reduce Method
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-const deposits = movements.filter(function (mov, i, arr) {
-  return mov > 0;
-});
+
 console.log(movements);
-console.log(deposits);
 
-const depositsFor = [];
-for (const mov of movements) if (mov > 0) depositsFor.push(mov);
-console.log(depositsFor);
+// accumulator -> SNOWBALL
+// const balance = movements.reduce(function (acc, cur, i, arr) {
+//   console.log(`Iteration ${i}: ${acc}`);
+//   return acc + cur;
+// }, 0);
+const balance = movements.reduce((acc, cur) => acc + cur, 0);
+console.log(balance);
 
-const withdrawals = movements.filter(mov => mov < 0);
-console.log(withdrawals);
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
+
+// Maximum value
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+console.log(max);
+
+const max2 = movements.reduce((acc, mov) => (acc > mov ? acc : mov));
+console.log(`max ${max2}`);
 
 /////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////

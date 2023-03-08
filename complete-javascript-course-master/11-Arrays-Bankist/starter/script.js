41847e711725a23aea6e01806aca23d5cd42ddc3
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-03-08 11:54:42 +0000

    add calcDisplaySummary out

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -290,6 +290,11 @@ const calcDisplaySummary = function (movement) {
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumIn.textContent = `${incomes} €`;
+
+  const out = movement
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)} €`;
 };
 
 calcDisplaySummary(account1.movements);

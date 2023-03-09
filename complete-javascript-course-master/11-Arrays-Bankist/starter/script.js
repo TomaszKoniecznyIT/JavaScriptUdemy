111882e2454be57d7ddadec92024b8d848024459
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-03-09 13:01:00 +0000

    adding loan function

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -376,6 +376,22 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+// 161
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
 // 160
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();

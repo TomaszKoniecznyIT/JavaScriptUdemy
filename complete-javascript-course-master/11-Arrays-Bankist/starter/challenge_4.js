6554b44961f7ffa5d2fd4232b2404fe87b2c20e0
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-03-12 13:46:05 +0000

    challenge 4 Jonas

--- complete-javascript-course-master/11-Arrays-Bankist/starter/challenge_4.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/challenge_4.js
@@ -60,3 +60,52 @@ const sortedDogs = dogs
   .slice()
   .sort((a, b) => a.recommendedFood - b.recommendedFood);
 console.log(sortedDogs);
+
+// Jonas
+// // 1.
+// dogs.forEach(dog => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28)));
+
+// // 2.
+// const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+// console.log(dogSarah);
+// console.log(
+//   `Sarah's dog is eating too ${
+//     dogSarah.curFood > dogSarah.recFood ? 'much' : 'little'
+//   } `
+// );
+
+// // 3.
+// const ownersEatTooMuch = dogs
+//   .filter(dog => dog.curFood > dog.recFood)
+//   .flatMap(dog => dog.owners);
+// // .flat();
+// console.log(ownersEatTooMuch);
+
+// const ownersEatTooLittle = dogs
+//   .filter(dog => dog.curFood < dog.recFood)
+//   .flatMap(dog => dog.owners);
+// console.log(ownersEatTooLittle);
+
+// // 4.
+// // "Matilda and Alice and Bob's dogs eat too much!"
+// //  "Sarah and John and Michael's dogs eat too little!"
+// console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much!`);
+// console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little!`);
+
+// // 5.
+// console.log(dogs.some(dog => dog.curFood === dog.recFood));
+
+// // 6.
+// // current > (recommended * 0.90) && current < (recommended * 1.10)
+// const checkEatingOkay = dog =>
+//   dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1;
+
+// console.log(dogs.some(checkEatingOkay));
+
+// // 7.
+// console.log(dogs.filter(checkEatingOkay));
+
+// // 8.
+// // sort it by recommended food portion in an ascending order [1,2,3]
+// const dogsSorted = dogs.slice().sort((a, b) => a.recFood - b.recFood);
+// console.log(dogsSorted);

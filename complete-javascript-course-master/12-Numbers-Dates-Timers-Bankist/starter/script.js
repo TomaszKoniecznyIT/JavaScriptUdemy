c467fa08d75e51a40270e9b8d877e0d3df889ee0
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-03-14 10:37:46 +0000

    BigInt

--- complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -360,21 +360,54 @@ btnSort.addEventListener('click', function (e) {
 //   });
 // });
 
+// ///////////////////////////////////////
+// //173. Numeric Separators
+
+// // 287,460,000,000
+// const diameter = 287_460_000_000;
+// console.log(diameter);
+
+// const price = 345_99;
+// console.log(price);
+
+// const transferFee1 = 15_00;
+// const transferFee2 = 1_500;
+
+// const PI = 3.1415;
+// console.log(PI);
+
+// console.log(Number('230_000'));
+// console.log(parseInt('230_000'));
+
 ///////////////////////////////////////
-// Numeric Separators
+// Working with BigInt
+console.log(2 ** 53 - 1);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(2 ** 53 + 1);
+console.log(2 ** 53 + 2);
+console.log(2 ** 53 + 3);
+console.log(2 ** 53 + 4);
 
-// 287,460,000,000
-const diameter = 287_460_000_000;
-console.log(diameter);
+console.log(4838430248342043823408394839483204n);
+console.log(BigInt(48384302));
 
-const price = 345_99;
-console.log(price);
+// Operations
+console.log(10000n + 10000n);
+console.log(36286372637263726376237263726372632n * 10000000n);
+// console.log(Math.sqrt(16n));
 
-const transferFee1 = 15_00;
-const transferFee2 = 1_500;
+const huge = 20289830237283728378237n;
+const num = 23;
+console.log(huge * BigInt(num));
 
-const PI = 3.1415;
-console.log(PI);
+// Exceptions
+console.log(20n > 15);
+console.log(20n === 20);
+console.log(typeof 20n);
+console.log(20n == '20');
 
-console.log(Number('230_000'));
-console.log(parseInt('230_000'));
+console.log(huge + ' is REALLY big!!!');
+
+// Divisions
+console.log(11n / 3n);
+console.log(10 / 3);

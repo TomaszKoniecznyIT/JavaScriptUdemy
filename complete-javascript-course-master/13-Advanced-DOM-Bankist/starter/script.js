7c94db40ac7868b7a4ded9d1dc826c5938d15137
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-03-17 13:25:39 +0000

    styles,sttributes and classes

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -34,7 +34,7 @@ document.addEventListener('keydown', function (e) {
 ///////////////////////////////////////
 
 ///////////////////////////////////////
-//186. Selecting, Creating, and Deleting Elements
+// //186. Selecting, Creating, and Deleting Elements
 
 // Selecting elements
 console.log(document.documentElement);
@@ -74,3 +74,57 @@ document
     message.remove();
     // message.parentElement.removeChild(message);
   });
+
+///////////////////////////////////////
+// Styles, Attributes and Classes
+
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '110%';
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+console.log(message.style.width);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+console.log(message.style.height);
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+console.log(logo.alt);
+
+// Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+console.log(logo.dataset.jakCo);
+
+// Classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c'); // not includes
+
+// // Don't use
+// logo.clasName = 'jonas';

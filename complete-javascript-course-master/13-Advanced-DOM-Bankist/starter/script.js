0602ca7c539c0c65c2c864a746f90f896444c95a
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-03-23 11:22:49 +0000

    lazy loading images

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -130,6 +130,32 @@ allSections.forEach(function (section) {
   section.classList.add('section--hidden');
 });
 
+//199
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
 // ///////////////////////////////////////
 // ///////////////////////////////////////
 
@@ -454,3 +480,29 @@ allSections.forEach(function (section) {
 //   sectionObserver.observe(section);
 //   section.classList.add('section--hidden');
 // });
+
+// //199. Lazy loading images
+// const imgTargets = document.querySelectorAll('img[data-src]');
+
+// const loadImg = function (entries, observer) {
+//   const [entry] = entries;
+
+//   if (!entry.isIntersecting) return;
+
+//   // Replace src with data-src
+//   entry.target.src = entry.target.dataset.src;
+
+//   entry.target.addEventListener('load', function () {
+//     entry.target.classList.remove('lazy-img');
+//   });
+
+//   observer.unobserve(entry.target);
+// };
+
+// const imgObserver = new IntersectionObserver(loadImg, {
+//   root: null,
+//   threshold: 0,
+//   rootMargin: '200px',
+// });
+
+// imgTargets.forEach(img => imgObserver.observe(img));

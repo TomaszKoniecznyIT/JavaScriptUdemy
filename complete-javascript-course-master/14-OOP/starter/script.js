0402c6b61c9d303340b954508ef8e1392909acab
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-03-27 14:22:24 +0100

    ES6 Classes

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,77 +1,164 @@
 'use strict';
 
+// ///////////////////////////////////////
+// //208. Constructor Functions and the new Operator
+// const Person = function (firstName, birthYear) {
+//   // Instance properties
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+
+//   // Never to this!
+//   // this.calcAge = function () {
+//   //   console.log(2037 - this.birthYear);
+//   // };
+// };
+
+// const jonas = new Person('Jonas', 1991);
+// console.log(jonas);
+
+// // 1. New {} is created
+// // 2. function is called, this = {}
+// // 3. {} linked to prototype
+// // 4. function automatically return {}
+
+// const matilda = new Person('Matilda', 2017);
+// const jack = new Person('Jack', 1975);
+
+// console.log(jonas instanceof Person);
+
+// ///////////////////////////////////////
+// //209. Prototypes
+// console.log(Person.prototype);
+
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
+
+// jonas.calcAge();
+// matilda.calcAge();
+
+// console.log(jonas.__proto__);
+// console.log(jonas.__proto__ === Person.prototype);
+
+// console.log(Person.prototype.isPrototypeOf(jonas));
+// console.log(Person.prototype.isPrototypeOf(matilda));
+// console.log(Person.prototype.isPrototypeOf(Person));
+// // .prototyeOfLinkedObjects
+// console.log(Person.prototype.__proto__);
+
+// Person.prototype.species = 'Homo Sapiens';
+// console.log(jonas.species, matilda.species);
+
+// console.log(jonas.hasOwnProperty('firstName'));
+// console.log(jonas.hasOwnProperty('species'));
+
+// ///////////////////////////////////////
+// //211. Prototypal Inheritance on Built-In Objects
+// console.log(jonas.__proto__);
+// // Object.prototype (top of prototype chain)
+// console.log(jonas.__proto__.__proto__);
+// console.log(jonas.__proto__.__proto__.__proto__);
+
+// console.dir(Person.prototype.constructor);
+
+// const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
+// console.log(arr.__proto__);
+// console.log(arr.__proto__ === Array.prototype);
+
+// console.log(arr.__proto__.__proto__);
+
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
+
+// console.log(arr.unique());
+
+// const h1 = document.querySelector('h1');
+// console.dir(x => x + 1);
+
+// ///////////////////////////////////////
+// //212. Jonas Coding Challenge #1
+
+// /*
+// 1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+// 2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+// 3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+// 4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+// DATA CAR 1: 'BMW' going at 120 km/h
+// DATA CAR 2: 'Mercedes' going at 95 km/h
+
+// GOOD LUCK 😀
+// */
+
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
+
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   console.log(`${this.make} is going at ${this.speed} km/h`);
+// };
+
+// Car.prototype.brake = function () {
+//   this.speed -= 5;
+//   console.log(`${this.make} is going at ${this.speed} km/h`);
+// };
+
+// const bmw = new Car('BMW', 120);
+// const mercedes = new Car('Mercedes', 95);
+
+// bmw.accelerate();
+// bmw.accelerate();
+// bmw.brake();
+// bmw.accelerate();
+
 ///////////////////////////////////////
-//208. Constructor Functions and the new Operator
-const Person = function (firstName, birthYear) {
-  // Instance properties
-  this.firstName = firstName;
-  this.birthYear = birthYear;
+//213. ES6 Classes
 
-  // Never to this!
-  // this.calcAge = function () {
-  //   console.log(2037 - this.birthYear);
-  // };
-};
+// Class expression
+// const PersonCl = class {}
 
-const jonas = new Person('Jonas', 1991);
-console.log(jonas);
+// Class declaration
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
 
-// 1. New {} is created
-// 2. function is called, this = {}
-// 3. {} linked to prototype
-// 4. function automatically return {}
+  // Instance methods
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
 
-const matilda = new Person('Matilda', 2017);
-const jack = new Person('Jack', 1975);
+  greet() {
+    console.log(`Hey ${this.fullName}`);
+  }
 
-console.log(jonas instanceof Person);
+  // Static method
+  static hey() {
+    console.log('Hey there 👋');
+    console.log(this);
+  }
+}
 
-///////////////////////////////////////
-//209. Prototypes
-console.log(Person.prototype);
+const jessica = new PersonCl('Jessica Davis', 1996);
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.age);
 
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear);
-};
+console.log(jessica.__proto__ === PersonCl.prototype);
 
-jonas.calcAge();
-matilda.calcAge();
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+jessica.greet();
 
-console.log(jonas.__proto__);
-console.log(jonas.__proto__ === Person.prototype);
+// 1. Classes are NOT hoisted
+// 2. Classes are first-class citizens
+// 3. Classes are executed in strict mode
 
-console.log(Person.prototype.isPrototypeOf(jonas));
-console.log(Person.prototype.isPrototypeOf(matilda));
-console.log(Person.prototype.isPrototypeOf(Person));
-// .prototyeOfLinkedObjects
-console.log(Person.prototype.__proto__);
-
-Person.prototype.species = 'Homo Sapiens';
-console.log(jonas.species, matilda.species);
-
-console.log(jonas.hasOwnProperty('firstName'));
-console.log(jonas.hasOwnProperty('species'));
-
-///////////////////////////////////////
-//211. Prototypal Inheritance on Built-In Objects
-console.log(jonas.__proto__);
-// Object.prototype (top of prototype chain)
-console.log(jonas.__proto__.__proto__);
-console.log(jonas.__proto__.__proto__.__proto__);
-
-console.dir(Person.prototype.constructor);
-
-const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
-console.log(arr.__proto__);
-console.log(arr.__proto__ === Array.prototype);
-
-console.log(arr.__proto__.__proto__);
-
-Array.prototype.unique = function () {
-  return [...new Set(this)];
-};
-
-console.log(arr.unique());
-
-const h1 = document.querySelector('h1');
-console.dir(x => x + 1);
+const walter = new PersonCl('Walter White', 1965);
+// PersonCl.hey();

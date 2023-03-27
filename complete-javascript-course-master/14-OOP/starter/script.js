e712e6ff0cc780702ecde2f5e9a9140439e406b9
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-03-27 14:34:28 +0100

    set and get

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -137,6 +137,20 @@ class PersonCl {
     console.log(`Hey ${this.fullName}`);
   }
 
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exists
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
   // Static method
   static hey() {
     console.log('Hey there 👋');
@@ -162,3 +176,23 @@ jessica.greet();
 
 const walter = new PersonCl('Walter White', 1965);
 // PersonCl.hey();
+
+///////////////////////////////////////
+//214. Setters and Getters
+const account = {
+  owner: 'Jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);

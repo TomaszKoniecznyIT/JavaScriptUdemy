7c9316fb16b4cc5b9373d1895e937dc1caedfbde
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-03-27 15:17:36 +0100

    Object.create

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -204,3 +204,28 @@ console.log(account.latest);
 
 account.latest = 50;
 console.log(account.movements);
+
+///////////////////////////////////////
+//216. Object.create
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge();
+
+console.log(steven.__proto__ === PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+sarah.calcAge();

168411ed69bbdb2723f59ef32846e046e2ab40f2
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-03-30 10:04:16 +0100

    leaflet

--- complete-javascript-course-master/15-Mapty/starter/script.js
+++ complete-javascript-course-master/15-Mapty/starter/script.js
@@ -21,6 +21,20 @@ if (navigator.geolocation)
       const { longitude } = position.coords;
       console.log(latitude, longitude);
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+      const coords = [latitude, longitude];
+
+      // 233. Displaying map
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your position');

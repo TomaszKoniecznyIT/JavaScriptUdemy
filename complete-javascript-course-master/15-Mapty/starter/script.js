a39365ddd77e8f60bdcdb2465ce18a89c9041566
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-03-30 10:27:29 +0100

    leaflet map marker 1

--- complete-javascript-course-master/15-Mapty/starter/script.js
+++ complete-javascript-course-master/15-Mapty/starter/script.js
@@ -15,28 +15,29 @@ const inputElevation = document.querySelector('.form__input--elevation');
 //232. Geolocation API
 
 if (navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      console.log(latitude, longitude);
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-      const coords = [latitude, longitude];
+  navigator.geolocation.getCurrentPosition(function (position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(latitude, longitude);
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    const coords = [latitude, longitude];
 
-      // 233. Displaying map
-      const map = L.map('map').setView(coords, 13);
+    // 233. Displaying map
+    const map = L.map('map').setView(coords, 13);
 
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
-    },
-    function () {
-      alert('Could not get your position');
-    }
-  );
+    // 234. Map Marker
+    map.on('click', function (mapEvent) {
+      console.log(mapEvent);
+      const { lat, lng } = mapEvent.latlng;
+
+      L.marker([lat, lng]).addTo(map).bindPopup('Workout').openPopup();
+    }),
+      function () {
+        alert('Could not get your position');
+      };
+  });

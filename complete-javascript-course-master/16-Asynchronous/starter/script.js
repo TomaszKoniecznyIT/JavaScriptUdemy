b9b58e3bdfc1e2904b1f0dba2e4963374398da11
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-04-11 12:52:17 +0100

    chaining promises

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -119,10 +119,29 @@ const getCountryAndNeighbour = function (country) {
 //     });
 // };
 
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v2/name/${country}`)
+//     .then(response => response.json())
+//     .then(data => renderCountry(data[0]));
+// };
+
+/////////////////////////////////
+// 253 Chaining promises
+
 const getCountryData = function (country) {
   fetch(`https://restcountries.com/v2/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
 };
 
-getCountryData('spain');
+getCountryData('poland');
+getCountryData('portugal');

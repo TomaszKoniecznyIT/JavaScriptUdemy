bde6463294c7e420388bf2440ae605b73d665060
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-04-11 17:30:14 +0100

    error

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -145,3 +145,93 @@ const getCountryData = function (country) {
 
 getCountryData('poland');
 getCountryData('portugal');
+
+// ///////////////////////////////////////
+// // Consuming Promises
+// // Chaining Promises
+// // Handling Rejected Promises
+// // Throwing Errors Manually
+
+// // const getCountryData = function (country) {
+// //   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+// //     .then(function (response) {
+// //       console.log(response);
+// //       return response.json();
+// //     })
+// //     .then(function (data) {
+// //       console.log(data);
+// //       renderCountry(data[0]);
+// //     });
+// // };
+
+// // const getCountryData = function (country) {
+// //   // Country 1
+// //   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+// //     .then(response => {
+// //       console.log(response);
+
+// //       if (!response.ok)
+// //         throw new Error(`Country not found (${response.status})`);
+
+// //       return response.json();
+// //     })
+// //     .then(data => {
+// //       renderCountry(data[0]);
+// //       // const neighbour = data[0].borders[0];
+// //       const neighbour = 'dfsdfdef';
+
+// //       if (!neighbour) return;
+
+// //       // Country 2
+// //       return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+// //     })
+// //     .then(response => {
+// //       if (!response.ok)
+// //         throw new Error(`Country not found (${response.status})`);
+
+// //       return response.json();
+// //     })
+// //     .then(data => renderCountry(data, 'neighbour'))
+// //     .catch(err => {
+// //       console.error(`${err} 💥💥💥`);
+// //       renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
+// //     })
+// //     .finally(() => {
+// //       countriesContainer.style.opacity = 1;
+// //     });
+// // };
+
+// const getCountryData = function (country) {
+//   // Country 1
+//   getJSON(
+//     `https://restcountries.eu/rest/v2/name/${country}`,
+//     'Country not found'
+//   )
+//     .then(data => {
+//       renderCountry(data[0]);
+//       const neighbour = data[0].borders[0];
+
+//       if (!neighbour) throw new Error('No neighbour found!');
+
+//       // Country 2
+//       return getJSON(
+//         `https://restcountries.eu/rest/v2/alpha/${neighbour}`,
+//         'Country not found'
+//       );
+//     })
+
+//     .then(data => renderCountry(data, 'neighbour'))
+//     .catch(err => {
+//       console.error(`${err} 💥💥💥`);
+//       renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
+
+// btn.addEventListener('click', function () {
+//   getCountryData('portugal');
+// });
+
+// // getCountryData('australia');

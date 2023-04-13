c8725cbdb1520d38a98a9b40ab9375223b44b4dc
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-04-13 15:05:58 +0100

    Promise.all or allSettled

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -595,17 +595,32 @@ const getJSON = function (url, errorMsg = 'Something went wrong') {
 //   console.log(res[0]);
 // })();
 
-const timeout = function (sec) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error('Request took too long!'));
-    }, sec * 1000);
-  });
-};
+// const timeout = function (sec) {
+//   return new Promise(function (_, reject) {
+//     setTimeout(function () {
+//       reject(new Error('Request took too long!'));
+//     }, sec * 1000);
+//   });
+// };
 
-Promise.race([
-  getJSON(`https://restcountries.com/v2/name/tanzania`),
-  timeout(0.01),
+// Promise.race([
+//   getJSON(`https://restcountries.com/v2/name/tanzania`),
+//   timeout(0.01),
+// ])
+//   .then(res => console.log(res[0]))
+//   .catch(err => console.error(err));
+
+// Promise.allSettled
+Promise.allSettled([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
+]).then(res => console.log(res));
+
+Promise.all([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
 ])
-  .then(res => console.log(res[0]))
+  .then(res => console.log(res))
   .catch(err => console.error(err));

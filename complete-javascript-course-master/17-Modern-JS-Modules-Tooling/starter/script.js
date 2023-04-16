dcc9a77c78b533f8d57c3a4f39fdd0f74205695b
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-04-16 20:54:19 +0100

    top level await ES2022

--- complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,3 +1,6 @@
+///////////////////////////////////////
+//272. Exporting and Importing in ES6 Modules
+
 // import { addCart, totalPrice as price, qt } from './shoppingCart.js';
 console.log('Importing module');
 
@@ -17,3 +20,28 @@ console.log(ShopCart.cart);
 add('bread', 3);
 
 console.log(ShopCart.cart);
+
+///////////////////////////////////////
+//273. Top-Level Await (ES2022)
+
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// Not very clean
+// lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
--- complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -1,5 +1,10 @@
 console.log('Exporting module');
 
+// // Blocking code
+// console.log('Start fetching users');
+// await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('Finish fetching users');
+
 const shippingCost = 10;
 export const cart = [];
 

ed8b9e34d0f53682a937c07fadb3972910054c0d
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-04-17 10:57:42 +0100

    npm

--- complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,83 +1,83 @@
-///////////////////////////////////////
-//272. Exporting and Importing in ES6 Modules
+// ///////////////////////////////////////
+// //272. Exporting and Importing in ES6 Modules
 
-// import { addCart, totalPrice as price, qt } from './shoppingCart.js';
-console.log('Importing module');
+// // import { addCart, totalPrice as price, qt } from './shoppingCart.js';
+// console.log('Importing module');
 
-// addCart('bread', 5);
-// console.log(price);
-// console.log(qt);
+// // addCart('bread', 5);
+// // console.log(price);
+// // console.log(qt);
 
-import * as ShopCart from './shoppingCart.js';
-console.log(ShopCart.qt);
-console.log(ShopCart.totalPrice);
+// import * as ShopCart from './shoppingCart.js';
+// console.log(ShopCart.qt);
+// console.log(ShopCart.totalPrice);
 
-import add from './shoppingCart.js';
-add('pizza', 2);
+// import add from './shoppingCart.js';
+// add('pizza', 2);
 
-console.log(ShopCart.cart);
+// console.log(ShopCart.cart);
 
-add('bread', 3);
+// add('bread', 3);
 
-console.log(ShopCart.cart);
+// console.log(ShopCart.cart);
 
-///////////////////////////////////////
-//273. Top-Level Await (ES2022)
+// ///////////////////////////////////////
+// //273. Top-Level Await (ES2022)
 
-// console.log('Start fetching');
-// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-// const data = await res.json();
-// console.log(data);
-// console.log('Something');
+// // console.log('Start fetching');
+// // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// // const data = await res.json();
+// // console.log(data);
+// // console.log('Something');
 
-const getLastPost = async function () {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const data = await res.json();
+// const getLastPost = async function () {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//   const data = await res.json();
 
-  return { title: data[data.length - 1].title, text: data.at(-1).body };
-};
+//   return { title: data[data.length - 1].title, text: data.at(-1).body };
+// };
 
-const lastPost = getLastPost();
-console.log(lastPost);
+// const lastPost = getLastPost();
+// console.log(lastPost);
 
-// Not very clean
-// lastPost.then(last => console.log(last));
+// // Not very clean
+// // lastPost.then(last => console.log(last));
 
-const lastPost2 = await getLastPost();
-console.log(lastPost2);
+// const lastPost2 = await getLastPost();
+// console.log(lastPost2);
 
-///////////////////////////////////////
-//274. The Module Pattern
+// ///////////////////////////////////////
+// //274. The Module Pattern
 
-const ShoppingCart2 = (function () {
-  const cart = [];
-  const shippingCost = 10;
-  const totalPrice = 237;
-  const totalQuantity = 23;
+// const ShoppingCart2 = (function () {
+//   const cart = [];
+//   const shippingCost = 10;
+//   const totalPrice = 237;
+//   const totalQuantity = 23;
 
-  const addToCart = function (product, quantity) {
-    cart.push({ product, quantity });
-    console.log(
-      `${quantity} ${product} added to cart (sipping cost is ${shippingCost})`
-    );
-  };
+//   const addToCart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(
+//       `${quantity} ${product} added to cart (sipping cost is ${shippingCost})`
+//     );
+//   };
 
-  const orderStock = function (product, quantity) {
-    console.log(`${quantity} ${product} ordered from supplier`);
-  };
+//   const orderStock = function (product, quantity) {
+//     console.log(`${quantity} ${product} ordered from supplier`);
+//   };
 
-  return {
-    addToCart,
-    cart,
-    totalPrice,
-    totalQuantity,
-  };
-})();
+//   return {
+//     addToCart,
+//     cart,
+//     totalPrice,
+//     totalQuantity,
+//   };
+// })();
 
-ShoppingCart2.addToCart('apple', 4);
-ShoppingCart2.addToCart('pizza', 2);
-console.log(ShoppingCart2);
-console.log(ShoppingCart2.shippingCost);
+// ShoppingCart2.addToCart('apple', 4);
+// ShoppingCart2.addToCart('pizza', 2);
+// console.log(ShoppingCart2);
+// console.log(ShoppingCart2.shippingCost);
 
 // ///////////////////////////////////////
 // //275. CommonJS Modules
@@ -91,3 +91,23 @@ console.log(ShoppingCart2.shippingCost);
 
 //   // Import
 //   const { addTocart } = require('./shoppingCart.js');
+
+///////////////////////////////////////
+//277. Introduction to NPM
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);

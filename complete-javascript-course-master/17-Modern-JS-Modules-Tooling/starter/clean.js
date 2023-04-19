ebdc2d5683dcfc5c41492ad3c6c2114b83b406d0
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-04-19 10:56:42 +0100

    fix some bad code

--- complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/clean.js
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -1,4 +1,7 @@
-var budget = [
+// 281
+
+// var to const or let
+const budget = [
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -9,56 +12,80 @@ var budget = [
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
 ];
 
-var limits = {
+const spendingLimits = {
   jonas: 1500,
   matilda: 100,
 };
 
-var add = function (value, description, user) {
-  if (!user) user = 'jonas';
+const getLimit = user => spendingLimits?.[user] ?? 0;
+
+const addExpense = function (value, description, user = 'jonas') {
   user = user.toLowerCase();
 
-  var lim;
-  if (limits[user]) {
-    lim = limits[user];
-  } else {
-    lim = 0;
-  }
+  // let lim;
+  // if (spendingLimits[user]) {
+  //   lim = spendingLimits[user];
+  // } else {
+  //   lim = 0;
+  // }
+  // const limit = spendingLimits[user] ? spendingLimits[user] : 0; // 1.
+  // const limit = spendingLimits?.[user] ?? 0;
+  // const limit = getLimit(user);
 
-  if (value <= lim) {
-    budget.push({ value: -value, description: description, user: user });
-  }
-};
-add(10, 'Pizza 🍕');
-add(100, 'Going to movies 🍿', 'Matilda');
-add(200, 'Stuff', 'Jay');
-console.log(budget);
-
-var check = function () {
-  for (var el of budget) {
-    var lim;
-    if (limits[el.user]) {
-      lim = limits[el.user];
-    } else {
-      lim = 0;
-    }
-
-    if (el.value < -lim) {
-      el.flag = 'limit';
-    }
+  // if (value <= limit) {
+  //   budget.push({ value: -value, description: description, user: user });
+  // }
+  if (value <= getLimit(user)) {
+    budget.push({ value: -value, description, user });
   }
 };
-check();
+addExpense(10, 'Pizza 🍕');
+addExpense(100, 'Going to movies 🍿', 'Matilda');
+addExpense(200, 'Stuff', 'Jay');
 
-console.log(budget);
+// const checkExpeses = function () {
+//   for (const entry of budget) {
+//     let lim;
+//     if (spendingLimits[entry.user]) {
+//       lim = spendingLimits[entry.user];
+//     } else {
+//       lim = 0;
+//     }
 
-var bigExpenses = function (limit) {
-  var output = '';
-  for (var el of budget) {
-    if (el.value <= -limit) {
-      output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
-    }
-  }
+//     if (entry.value < -lim) {
+//       entry.flag = 'limit';
+//     }
+//   }
+// };
+const checkExpeses = function () {
+  // const limit = spendingLimits?.[entry.user] ?? 0;
+
+  for (const entry of budget)
+    if (entry.value < -getLimit(entry.user)) entry.flag = 'limit';
+};
+checkExpeses();
+
+// const logBigExpenses = function (biglimit) {
+//   let output = '';
+//   for (const el of budget) {
+//     if (el.value <= -biglimit) {
+//       output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
+//     }
+//   }
+//   output = output.slice(0, -2); // Remove last '/ '
+//   console.log(output);
+// };
+
+const logBigExpenses = function (biglimit) {
+  let output = '';
+  for (const entry of budget)
+    output +=
+      entry.value <= -biglimit ? `${entry.description.slice(-2)} / ` : ''; // Emojis are 2 chars
+
   output = output.slice(0, -2); // Remove last '/ '
   console.log(output);
 };
+
+console.log(budget);
+logBigExpenses(1000);
+for (const entry of budget) console.log(entry);

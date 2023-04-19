8ce511cd240fb0cbd1e24cd0a3437a954ca11280
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-04-19 09:26:19 +0100

    babel and polyfilling

--- complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -92,22 +92,45 @@
 //   // Import
 //   const { addTocart } = require('./shoppingCart.js');
 
-///////////////////////////////////////
-//277. Introduction to NPM
+// ///////////////////////////////////////
+// //277. Introduction to NPM
 
-import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+// import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
 
-const state = {
-  cart: [
-    { product: 'bread', quantity: 5 },
-    { product: 'pizza', quantity: 5 },
-  ],
-  user: { loggedIn: true },
-};
-const stateClone = Object.assign({}, state);
-const stateDeepClone = cloneDeep(state);
+// const state = {
+//   cart: [
+//     { product: 'bread', quantity: 5 },
+//     { product: 'pizza', quantity: 5 },
+//   ],
+//   user: { loggedIn: true },
+// };
+// const stateClone = Object.assign({}, state);
+// const stateDeepClone = cloneDeep(state);
 
-state.user.loggedIn = false;
-console.log(stateClone);
+// state.user.loggedIn = false;
+// console.log(stateClone);
 
-console.log(stateDeepClone);
+// console.log(stateDeepClone);
+
+//280
+
+// class Person {
+//   #greeting = 'Hey';
+//   constructor(name) {
+//     this.name = name;
+//     console.log(`${this.#greeting}, ${this.name}`);
+//   }
+// }
+// const jonas = new Person('Jonas');
+
+// console.log('Jonas' ?? null);
+
+// console.log(cart.find(el => el.quantity >= 2));
+// Promise.resolve('TEST').then(x => console.log(x));
+
+// import 'core-js/stable';
+// // import 'core-js/stable/array/find';
+// // import 'core-js/stable/promise';
+
+// // Polifilling async functions
+// import 'regenerator-runtime/runtime';

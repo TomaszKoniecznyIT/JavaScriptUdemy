63a99737ccfde684bffab33541de78d2e91f858d
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-04-25 11:33:14 +0100

    loading recipe from API 1

--- complete-javascript-course-master/18-forkify/dist/index.e37f48ea.js
+++ complete-javascript-course-master/18-forkify/dist/index.e37f48ea.js
@@ -567,7 +567,18 @@ const timeout = function(s) {
 };
 // https://forkify-api.herokuapp.com/v2
 ///////////////////////////////////////
-fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886");
+const showRecipe = async function() {
+    try {
+        const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886");
+        const data = await res.json();
+        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+        console.log(res);
+        console.log(data);
+    } catch (err) {
+        alert(err);
+    }
+};
+showRecipe();
 
 },{}]},["d8XZh","aenu9"], "aenu9", "parcelRequire3a11")
 
--- complete-javascript-course-master/18-forkify/src/js/controller.js
+++ complete-javascript-course-master/18-forkify/src/js/controller.js
@@ -11,7 +11,20 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
 
-fetch(
-  'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
-);
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    console.log(res);
+    console.log(data);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();

aef4ca881ecd4c2a6b4375d94bcf08f4ed4482a3
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-05-10 13:31:11 +0100

    303 search correction

--- complete-javascript-course-master/18-forkify/dist/index.e37f48ea.js
+++ complete-javascript-course-master/18-forkify/dist/index.e37f48ea.js
@@ -2687,6 +2687,7 @@ const loadSearchResults = async function(query) {
                 image: rec.image_url
             };
         });
+        state.search.page = 1;
     } catch (err) {
         console.error(`${err}`);
         throw err;
--- complete-javascript-course-master/18-forkify/src/js/model.js
+++ complete-javascript-course-master/18-forkify/src/js/model.js
@@ -48,6 +48,7 @@ export const loadSearchResults = async function (query) {
         image: rec.image_url,
       };
     });
+    state.search.page = 1;
   } catch (err) {
     console.error(`${err}`);
     throw err;
